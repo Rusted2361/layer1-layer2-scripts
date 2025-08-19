890a37c0a1c3c7faf2b5d89a23b124b032ddbabe
Rusted2361 <hamzasajid4040@gmail.com>
2025-08-19 18:02:51 +0500

    give command for proposer execution

---
+++ run-proposer.sh
@@ -0,0 +1,30 @@
+cd layer2/proposer-node
+
+echo "pwd: $(pwd)"
+source .env
+cat <<EOF > scripts/start-proposer.sh
+#!/bin/bash
+ 
+source .env
+ 
+# Path to the op-proposer binary we built
+../optimism/op-proposer/bin/op-proposer \
+  --poll-interval=$POLL_INTERVAL \
+  --rpc.port=$PROPOSER_RPC_PORT \
+  --rpc.enable-admin \
+  --rollup-rpc=$ROLLUP_RPC_URL \
+  --l1-eth-rpc=$L1_RPC_URL \
+  --private-key=$GS_PROPOSER_PRIVATE_KEY \
+  --game-factory-address=$GAME_FACTORY_ADDRESS \
+  --game-type=$GAME_TYPE \
+  --proposal-interval=$PROPOSAL_INTERVAL \
+  --num-confirmations=1 \
+  --resubmission-timeout=30s \
+  --wait-node-sync=true \
+  --log.level=info
+EOF
+
+chmod +x scripts/start-proposer.sh
+
+echo "please run below command to start proposer"
+echo "./scripts/start-proposer.sh"
