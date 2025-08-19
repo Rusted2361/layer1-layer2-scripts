ba41377df61555609506d6788182381181be5b49
Rusted2361 <hamzasajid4040@gmail.com>
2025-08-19 18:03:18 +0500

    gives command for batcher execution

---
+++ run-batcher.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cd layer2/batcher-node
+
+echo "pwd: $(pwd)"
+source .env
+
+cat <<EOF > scripts/start-batcher.sh
+#!/bin/bash
+ 
+source .env
+ 
+# Path to the op-batcher binary we built
+../optimism/op-batcher/bin/op-batcher \
+  --l2-eth-rpc=$L2_RPC_URL \
+  --rollup-rpc=$ROLLUP_RPC_URL \
+  --poll-interval=$POLL_INTERVAL \
+  --sub-safety-margin=$SUB_SAFETY_MARGIN \
+  --num-confirmations=$NUM_CONFIRMATIONS \
+  --safe-abort-nonce-too-low-count=$SAFE_ABORT_NONCE_TOO_LOW_COUNT \
+  --resubmission-timeout=$RESUBMISSION_TIMEOUT \
+  --rpc.addr=0.0.0.0 \
+  --rpc.port=$BATCHER_RPC_PORT \
+  --rpc.enable-admin \
+  --max-channel-duration=$MAX_CHANNEL_DURATION \
+  --l1-eth-rpc=$L1_RPC_URL \
+  --private-key=$BATCHER_PRIVATE_KEY \
+  --batch-type=1 \
+  --data-availability-type=calldata \
+  --throttle-threshold=0 \
+  --throttle-always-block-size=0 \
+  --log.level=info
+EOF
+
+chmod +x scripts/start-batcher.sh
+
+echo "please run below command to start batcher"
+echo "./scripts/start-batcher.sh"
