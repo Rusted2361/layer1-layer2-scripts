febd29168203e60d49e6e610a43f65f939869a9f
Rusted2361 <hamzasajid4040@gmail.com>
2025-08-21 16:41:45 +0500

    added layer2 clean script.sh

--- clean-layer2.sh
+++ clean-layer2.sh
@@ -1,2 +1,5 @@
-remove .op-deployer cache from root of scripts-for-layer1-layer2 directory.
-remove op-geth-data from sequencer-node directory
+#!/usr/bin/env bash
+pgrep -f geth         | xargs -r kill -9
+pgrep -f op-node | xargs -r kill -9
+pgrep -f op-batcher    | xargs -r kill -9
+pgrep -f op-proposer    | xargs -r kill -9
