d37c8b9740e9b9858759760bbf89607395e870aa
Rusted2361 <hamzasajid4040@gmail.com>
2025-08-22 14:14:29 +0500

    added clean scripts

--- kill-l1.sh
+++ kill-l1.sh
@@ -2,3 +2,5 @@
 pgrep -f geth         | xargs -r kill -9
 pgrep -f beacon-chain | xargs -r kill -9
 pgrep -f validator    | xargs -r kill -9
+
+ rm -rf layer1/validatordata layer1/beacondata layer1/gethdata layer1/chain.log layer1/validator.log layer1/geth.log layer1/genesis.ssz
