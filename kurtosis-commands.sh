f23d17bb65574fe5ac4d0436950d3f0183bf803c
Rusted2361 <hamzasajid4040@gmail.com>
2025-09-08 19:42:50 +0500

    storage persistance

--- kurtosis-commands.sh
+++ kurtosis-commands.sh
@@ -8,3 +8,7 @@ kurtosis service logs my-testnet cl-1-prysm-geth --follow
 kurtosis service logs my-testnet el-1-geth-prysm --follow
 # watch logs of prysm validator
 kurtosis service logs my-testnet vc-1-geth-prysm --follow
+#enter to docker container
+kurtosis service shell my-testnet cl-1-prysm-geth
+kurtosis service shell my-testnet el-1-geth-prysm
+kurtosis service shell my-testnet vc-1-geth-prysm
